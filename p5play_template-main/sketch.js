8f63e8fc52b5966cb6ae4efa66e0e57cd0165fb3
vinifeliz <94203164+vinifeliz@users.noreply.github.com>
2021-11-12 19:53:35 -0300

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,21 @@
+var bloc
+function setup() {
+  createCanvas(500,500);
+  bloc=createSprite(100,100,50,50)
+  bloc.shapeColor = "green"
+  bloc.velocityX = 10
+}
+
+function draw() 
+{
+  
+  background("white");
+  drawSprites();
+
+  
+  
+}
+
+
+
+
